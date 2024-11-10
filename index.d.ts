a144fe737859b5402590eabeca6b4c5f9e6d23e0
Ammar <armmarov@gmail.com>
2024-11-10 15:43:01 +0800

    update code

--- index.d.ts
+++ index.d.ts
@@ -24,28 +24,97 @@ declare namespace ZtxChainSDK {
   interface Util {}
 
   interface Account {
+    create(): Response
+
+    checkValid(address: string): Response
+
+    getInfo(address: string): Response
+
+    getBalance(address: string): Response
+
     getNonce(address: string): Response
+
+    getAssets(address: string): Response
+
+    getMetadata(args: any): Response
+
+    isActivated(address: string): Response
   }
 
   interface Transaction {
-    evaluateFee(args: any): Response
-
     buildBlob(args: any): Response
 
     sign(args: any): Response
 
     submit(args: any): Response
 
+    evaluateFee(args: any): Response
+
     getInfo(hash: string): Response
   }
 
-  interface Token {}
+  interface Token {
+    getInfo(args: any): Response
+  }
 
-  interface Block {}
+  interface Block {
+    getNumber(): Response
 
-  interface Operation {}
+    checkStatus(): Response
 
-  interface Contract {}
+    getTransactions(blockNumber: number): Response
+
+    getInfo(blockNumber: number): Response
+
+    getLatestInfo(): Response
+
+    getValidators(blockNumber: number): Response
+
+    getLatestValidators(): Response
+
+    getLatestReward(): Response
+
+    getFees(blockNumber: number): Response
+
+    getLatestFees(): Response
+  }
+
+  interface Operation {
+    // Account
+    accountActivateOperation(args: any): Response
+
+    accountSetMetadataOperation(args: any): Response
+
+    accountSetPrivilegeOperation(args: any): Response
+
+    // Asset
+    assetIssueOperation(args: any): Response
+
+    assetSendOperation(args: any): Response
+
+    // Contract
+    contractCreateOperation(args: any): Response
+
+    contractInvokeByAssetOperation(args: any): Response
+
+    contractInvokeByGasOperation(args: any): Response
+
+    // Gas
+    gasSendOperation(args: any): Response
+
+    // Logs
+    logCreateOperation(args: any): Response
+  }
+
+  interface Contract {
+    getInfo(contractAddress: string): Response
+
+    checkValid(contractAddress: string): Response
+
+    call(args: any): Response
+
+    getAddress(hash: string): Response
+  }
 
   interface Response {
     result: any
